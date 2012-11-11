7d963090503598652bd16d3c7f09c457333cfaa5
Nils Schweinsberg <mail@n-sch.de>
2012-11-11 18:56:05 +0100

    compo/vector: remove functions that possibly leak mem

--- src/vector.h
+++ src/vector.h
@@ -25,19 +25,20 @@ typedef struct {
 #define VectorDIV(V, i, v) (VectorSET((V), (i), VectorGET((V), (i)) / (v)))
 
 vector null_vector(int N);
+vector vector_copy(const vector v);
 
 void vector_destroy(vector v);
 
-vector vector_add(const vector v1, const vector v2);
-void   vector_add_to(vector v1, const vector v2);
+//vector vector_add(const vector v1, const vector v2);
+void   vector_add(vector v1, const vector v2);
 
-vector vector_diff(const vector v1, const vector v2);
-void   vector_diff_from(vector v1, const vector v2);
+//vector vector_diff(const vector v1, const vector v2);
+void   vector_diff(vector v1, const vector v2);
 
-vector vector_cross_prod(const vector v1, const vector v2);
+//vector vector_cross_prod(const vector v1, const vector v2);
 double vector_mult(const vector v1, const vector v2);
 
-vector scalar_mult(double a, const vector v);
-void   scalar_mult_to(double a, vector v);
+//vector scalar_mult(double a, const vector v);
+void   scalar_mult(double a, vector v);
 
 double vector_abs(const vector v);
